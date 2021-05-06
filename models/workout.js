79dc928c1538cbcd22ab08ce5fa5c2cc0eab5a9a
Alaina Hicks <ajhicks0816@gmail.com>
2021-05-06 11:00:45 -0500

    models

---
+++ models/workout.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+            {
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise name"
+                },
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise type"
+                },
+                weight: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                duration: {
+                    type: Number,
+                    required: "Enter an exercise duration in minutes"
+                },
+                distance: {
+                    type: Number
+            }
+            }
+        ]
+    },
+{
+    toJSON: {
+        // include any virtual properties when data is requested
+        virtuals: true
+    }
+}
+);
+
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+    // "reduce" array of exercises down to just the sum of their durations
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
